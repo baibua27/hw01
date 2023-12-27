5dd3ab2d6e590c9cc0ae2836b0c765ce7b00845c
baibua27 <151831254+baibua27@users.noreply.github.com>
2023-12-27 22:18:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App/>)
 
-function Counter({ item: { id, number }, hdlUpdate, hdlRemove }) {
+// function Counter(props) {
+//   const {item : {id, number}, hdlUpdate} = props
+function Counter(props) {
+  console.log(props.item);
+
+  const updateCounter = (n) => {
+    if (props.item.number + n < 0) {
+      return;
+    }
+    props.hdlUpdate(props.item.id, n);
+  };
+
+  const removeCounter = () => {
+    // Call the hdlRemoveCounter function passed from App
+    props.hdlRemoveCounter(props.item.id);
+  };
+
   return (
     <div className='counter'>
-      <button onClick={() => hdlUpdate(id, -1)}> - </button>
-      <h3>{number}</h3>
-      <button onClick={() => hdlUpdate(id, 1)}> + </button>
-      <button onClick={() => hdlUpdate(id, -number)}> C </button>
-      <button onClick={() => hdlRemove(id)}> X </button>
+      <button onClick={() => updateCounter(-1)}> - </button>
+      <h2> {props.item.number} </h2>
+      <button onClick={() => updateCounter(1)}> + </button>
+      <button onClick={() => updateCounter(-props.item.number)}> C </button>
+      <button onClick={removeCounter}> X </button>
     </div>
   );
 }
 
+
 function SumInfo(props) {
-  const {sum, hdlAddCounter } = props;
+  const sum = props.counters.reduce((acc, counter) => acc + counter.number, 0);
+
+  const stTitle = {
+    color: props.color,
+    fontSize: props.size === 'big' ? '50px' : '40px',
+  };
 
   return (
     <div className='suminfo'>
-      <h1>Sum = {sum}</h1>
-      <button className='text-center' onClick={hdlAddCounter}>Add Counter</button>
+      <h1 style={stTitle}>Sum = {sum}</h1>
     </div>
   );
 }
@@ -29,38 +50,42 @@ function App() {
 
   const hdlUpdate = (id, num) => {
     const cloneCounters = [...counters];
-    const idx = cloneCounters.findIndex((el) => el.id === id);
-
-    if (cloneCounters[idx].number + num < 0) {
-      return;
-    }
-
+    let idx = cloneCounters.findIndex((el) => el.id === id);
     cloneCounters[idx].number += num;
     setCounters(cloneCounters);
   };
 
   const hdlAddCounter = () => {
-    const newId = counters.length === 0 ? 1 : counters[counters.length - 1].id + 1;
+    let newId = counters.length === 0 ? 1 : counters.at(-1).id + 1;
     const cloneCounters = [...counters];
     cloneCounters.push({ id: newId, number: 0 });
     setCounters(cloneCounters);
   };
 
   const hdlRemoveCounter = (id) => {
+    // Create a new array without the specified counter
     const updatedCounters = counters.filter((counter) => counter.id !== id);
     setCounters(updatedCounters);
   };
 
-  const sum = counters.reduce((acc, curr) => acc + curr.number, 0);
-
   return (
     <>
-      <SumInfo color="red" size="big" sum={sum} hdlAddCounter={hdlAddCounter} />
-      {counters.map((el) => (
-        <div key={el.id}>
-          <Counter item={el} hdlUpdate={hdlUpdate} hdlRemove={hdlRemoveCounter} />
-        </div>
-      ))}
+      <div className='suminfo'>
+        <h1 className='text-center'>Codecamp Academy 01</h1> <br /><br />
+        <button className='text-center' onClick={hdlAddCounter}>Add Counter</button>
+      </div>
+      <SumInfo color='red' size='big' counters={counters} />
+
+      {counters.map((el) => {
+        return (
+          <Counter
+            key={el.id}
+            item={el}
+            hdlUpdate={hdlUpdate}
+            hdlRemoveCounter={hdlRemoveCounter}
+          />
+        );
+      })}
     </>
   );
 }
